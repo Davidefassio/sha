e0969b1d81bf74cd2bcf46ee1444021a52553f39
Davide Fassio <55668928+Davidefassio@users.noreply.github.com>
2020-03-22 22:15:17 +0100

    all working

--- lib/sha_512.hpp
+++ lib/sha_512.hpp
@@ -1,8 +1,11 @@
 #ifndef SHA_512_HPP
 #define SHA_512_HPP
 
-void sha_512(char* input, char* output){
+#include <string>
+#include <fstream>
 
-}
+// Hashing function
+std::string sha_512(const std::string &, int);
+std::string sha_512(std::ifstream &, int);
 
 #endif /* SHA_512_HPP */
